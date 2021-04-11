a7252b187042d721452da996b826337733f8166d
izexlesh <81591222+izexleshJS@users.noreply.github.com>
2021-04-11 18:13:51 +0300

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -16,8 +16,8 @@ module.exports = client => {
   );
   client.user.setStatus("idle");
   var oyun = [
-    "Armor Code",
-    "Armor Code v12",
+    "İzex Code",
+    "İzexlesh V12 Boş Altyapı",
   
   ];
 
