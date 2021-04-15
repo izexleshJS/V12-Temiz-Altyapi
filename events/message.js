19ae37f0c44a05d6e561272560521b9950bce204
izexlesh <81591222+izexleshJS@users.noreply.github.com>
2021-04-15 16:24:53 +0300

    Update message.js

--- events/message.js
+++ events/message.js
@@ -1,13 +1,9 @@
-const Discord = require("discord.js");
-const ayarlar = require('../ayarlar.json');
-let talkedRecently = new Set();
-
 module.exports = message => {
   if (talkedRecently.has(message.author.id)) {
     return;
   }
   talkedRecently.add(message.author.id);
-	setTimeout(() => {
+    setTimeout(() => {
     talkedRecently.delete(message.author.id);
   }, 2500);
   let client = message.client;
@@ -23,61 +19,6 @@ module.exports = message => {
     cmd = client.commands.get(client.aliases.get(command));
   }
   if (cmd) {
-   if (cmd.conf.enabled === false) {
-      if (!ayarlar.sahip.includes(message.author.id) && !ayarlar.sahip.includes(message.author.id)) {
-        const embed = new Discord.RichEmbed()
-                    .setDescription(`:x: **${cmd.help.name}** isimli komut şuanda geçici olarak kullanıma kapalıdır!`)
-                    .setColor("RED")
-                message.channel.send({embed})
-                return
-      }
-    }
-    
-    if (cmd.conf.permLevel === 1) {
-			if (!message.member.hasPermission("MANAGE_MESSAGES")) {
-				const embed = new Discord.RichEmbed()
-					.setDescription(`Bu komutu kullanabilmek için **Mesajları Yönet** iznine sahip olmalısın!`)
-          .setColor("RED")
-				message.channel.send({embed})
-				return
-			}
-		}
-		if (cmd.conf.permLevel === 2) {
-			if (!message.member.hasPermission("KICK_MEMBERS")) {
-				const embed = new Discord.RichEmbed()
-					.setDescription(`Bu komutu kullanabilmek için **Üyeleri At** iznine sahip olmalısın!`)
-					.setColor("RED")
-				message.channel.send({embed})
-				return
-			}
-		}
-    if (cmd.conf.permLevel === 3) {
-			if (!message.member.hasPermission("BAN_MEMBERS")) {
-				const embed = new Discord.RichEmbed()
-					.setDescription(`Bu komutu kullanabilmek için **Üyeleri Yasakla** iznine sahip olmalısın!`)
-					.setColor("RED")
-				message.channel.send({embed})
-				return
-			}
-		}
-		if (cmd.conf.permLevel === 4) {
-			if (!message.member.hasPermission("ADMINISTRATOR")) {
-				const embed = new Discord.RichEmbed()
-					.setDescription(`Bu komutu kullanabilmek için **Yönetici** iznine sahip olmalısın!`)
-					.setColor("RED")
-				message.channel.send({embed})
-				return
-			}
-		}
-		if (cmd.conf.permLevel === 5) {
-			if (!ayarlar.sahip.includes(message.author.id)) {
-				const embed = new Discord.RichEmbed()
-					.setDescription(`Bu komutu sadece **sahibim** kullanabilir!`)
-					.setColor("RED")
-				message.channel.send({embed})
-				return
-			}
-		}
     if (perms < cmd.conf.permLevel) return;
     cmd.run(client, message, params, perms);
   }
